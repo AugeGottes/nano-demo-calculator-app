a8cbd1e105eb4285e5f5604ca910bf875312e637
AugeGottes <debanjandasrise@gmail.com>
2023-08-23 20:49:50 +0530

    Cpp 1

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,23 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello World!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int num1,num2;
+    num1=input["num1"].i();
+    num2=input["num2"].i();
+    int result=num1+num2;
+    return crow::response{to_string(result)};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int num1,num2;
+    num1=input["num1"].i();
+    num2=input["num2"].i();
+    int result=num1-num2;
+    return crow::response{to_string(result)};
 }
